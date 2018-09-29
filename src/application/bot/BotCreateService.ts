a029216b5acb6f830239246c935f5c794b1ca7b4
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-09-29 22:32:44 +0900

    実装。もともとの実装をコピペで持ってくる。 繋いだだけで、まだトランスパイル通らない。

--- src/application/bot/BotCreateService.ts
+++ src/application/bot/BotCreateService.ts
@@ -1,10 +1,15 @@
 import Bot from '../../domain/bot/Bot';
+import {
+  Client
+} from 'discord.js';
 
 export default class BotCreateService {
 
   constructor() {}
 
   public createBot(discordAppKey: string): Bot {
-    return new Bot();
+    // TODO DiscordClientを作る、キーを差し込む。
+    const client = new Client();
+    return new Bot(client);
   }
 }
--- src/domain/bot/Bot.ts
+++ src/domain/bot/Bot.ts
@@ -1,5 +1,39 @@
+import { Client, Message } from 'discord.js';
+
 export default class Bot {
-  public run() {
-    console.log('ここまでこれば御の字');
+  private readonly discordClient: Client;
+
+  constructor(discordClient: Client) {
+    this.discordClient = discordClient;
   }
+
+  public run() {
+    const client = this.discordClient;
+    client.on('ready', () => {
+      client.on('message',  (message: Message) => {
+        if (message.content.startsWith('/count')) {
+          let exists = false;
+          let num = 0;
+          client.channels.forEach((channel, id) => {
+            if (channel.type === 'voice') {
+              if (channel.members.has(message.author.id)) {
+                exists = true;
+                channel.members.forEach(function () {
+                  num = num + 1;
+                })
+              }
+            }
+          })
+          if (exists) {
+            var msg = '参加中のVCの接続人数: ' +  num;
+            message.channel.send(msg);
+          } else {
+            message.channel.send('VCにログインしていません.VCにログインの上コマンドを打ってください');
+          }
+        }
+      })
+    })
+  }
+
+
 }
