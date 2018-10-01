1e8e35c94f4aa48a411d5a9be3d2fc273ceb9c8c
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-10-01 23:45:20 +0900

    実装。ログインIDを与えるように変更。

--- src/application/bot/BotCreateService.ts
+++ src/application/bot/BotCreateService.ts
@@ -1,15 +1,13 @@
 import Bot from '../../domain/bot/Bot';
-import {
-  Client
-} from 'discord.js';
+import { Client } from 'discord.js';
 
 export default class BotCreateService {
 
   constructor() {}
 
   public createBot(discordAppKey: string): Bot {
-    // TODO DiscordClientを作る、キーを差し込む。
     const client = new Client();
+    client.login(discordAppKey);
     return new Bot(client);
   }
 }
